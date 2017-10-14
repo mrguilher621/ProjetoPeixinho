76cbdf0fd88838425dc6e70a71ade35df0ec0dd2
guilh <mrguilher621@gmail.com>
2017-10-14 17:32:50 -0300

    Novas Telas feitas

--- app/src/main/java/com/example/guilherme/projetopeixinho/Activitys/PrincipalActivity.java
+++ app/src/main/java/com/example/guilherme/projetopeixinho/Activitys/PrincipalActivity.java
@@ -1,15 +1,50 @@
 package com.example.guilherme.projetopeixinho.Activitys;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.guilherme.projetopeixinho.R;
 
 public class PrincipalActivity extends Activity {
 
+   private TextView TvAbreEtiqueta;
+    private  TextView TvAbreProduto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        TvAbreEtiqueta = (TextView) findViewById(R.id.tvAbreEtiqueta);
+        TvAbreEtiqueta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abreEtiquetas();
+            }
+        });
+
+        TvAbreProduto = (TextView) findViewById(R.id.tvAbreProduto);
+        TvAbreProduto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abreCadastroProduto();
+            }
+        });
+
+
+    }
+
+    public void abreCadastroProduto(){
+        Intent intent = new Intent(PrincipalActivity.this, CadastroProdutoActivity.class);
+        startActivity(intent);
+    }
+
+    public void abreEtiquetas(){
+        Intent intent = new Intent(PrincipalActivity.this, EtiquetasActivity.class);
+        startActivity(intent);
     }
 }
